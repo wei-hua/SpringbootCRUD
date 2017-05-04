118e091e25b92eeac95960d79d9a4a6763ac72e2
unknown <>
2017-05-04 11:42:48 +0800

    using mysql

--- src/main/java/com/websystique/springboot/SpringBootCRUDApp.java
+++ src/main/java/com/websystique/springboot/SpringBootCRUDApp.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
-import com.websystique.springboot.configuration.JpaConfiguration;
 
 
-@Import(JpaConfiguration.class)
 @SpringBootApplication(scanBasePackages={"com.websystique.springboot"})// same as @Configuration @EnableAutoConfiguration @ComponentScan
 public class SpringBootCRUDApp {
 
